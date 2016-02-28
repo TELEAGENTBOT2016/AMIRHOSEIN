d921fcad4b1d030d3168ca40c94067fd374b96b1
TELEAGENTBOT2016 <amirhoseinshamlou123456789@gmail.com>
2016-02-28 04:53:57 -0800

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,7 +245,8 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {140010873
+},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
